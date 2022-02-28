80cbf4647d5855f64d779ba178893d48443c83cb
David Yarhi <yarhi.david@gmail.com>
2022-02-28 17:35:30 +0900

    created seeds for users and comments using Faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,56 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "Destroying old Users..."
+User.destroy_all
+puts "Creating new Users..."
+
+20.times do
+  User.new(
+    name: Faker::FunnyName.unique.name,
+    email: Faker::Internet.unique.email,
+    password: "password"
+  )
+end
+
+puts "Destroying old comments..."
+Comment.destroy_all
+puts "Creating new comments..."
+
+100.times do
+  quote = rand(0..3)
+  loc_quote = quote
+  zero_or_one = rand(0..1)
+  amount = ""
+
+
+  case quote
+  when 0
+    Faker::TvShows::TwinPeaks.quote
+  when 1
+    Faker::Games::WarhammerFantasy.quote
+  when 2
+    Faker::Games::Overwatch.quote
+  when 3
+    Faker::Games::HeroesOfTheStorm.quote
+  end
+
+  case loc_quote
+  when 0
+    Faker::TvShows::TwinPeaks.location
+  when 1
+    Faker::Games::WarhammerFantasy.location
+  when 2
+    Faker::Games::Overwatch.location
+  when 3
+    Faker::Games::HeroesOfTheStorm.battleground
+  end
+
+  zero_or_one.zero? ? amount = "always" : amount = "never"
+
+  Comment.new(
+    content: "#{quote} This would #{amount} happen in #{loc_quote}. Complete #{Faker::Emotion.noun}.",
+    user_id: User.all.sample,
+    incident_id: Incident.all.sample
+  )
+end
