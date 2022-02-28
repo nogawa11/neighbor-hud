77f7231ddbd8bfcd643dd1aa79c5396a1619060a
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-02-28 13:43:54 +0900

    added mapbox geocode

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -1,5 +1,6 @@
 import { Controller } from "@hotwired/stimulus"
 import mapboxgl from "mapbox-gl"
+import MapboxGeocoder from "@mapbox/mapbox-gl-geocoder"
 
 export default class extends Controller {
   static values = {
@@ -12,7 +13,14 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/ayanorii/cl05huof2003o15nuivl7b2y7"
     })
+    // this.#addMarkersToMap()
+    // this.#fitMapToMarkers()
+
+    this.map.addControl(new MapboxGeocoder({
+      accessToken: mapboxgl.accessToken,
+      mapboxgl: mapboxgl
+    }))
   }
 }
