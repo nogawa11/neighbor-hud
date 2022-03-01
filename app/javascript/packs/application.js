e108a6f749f8397022e4454644dc3a5a5ed0f8f5
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-03-01 10:50:18 +0900

    registered flatpicker controller

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,7 +7,21 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import { Application } from 'stimulus'
+import { definitionsFromContext } from 'stimulus/webpack-helpers'
 
+const application = Application.start()
+const context = require.context('../controllers', true, /\.js$/)
+application.load(definitionsFromContext(context))
+
+// import Flatpickr
+import Flatpickr from 'stimulus-flatpickr'
+
+// Import style for flatpickr
+require("flatpickr/dist/flatpickr.css")
+
+// Manually register Flatpickr as a stimulus controller
+application.register('flatpickr', Flatpickr)
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
