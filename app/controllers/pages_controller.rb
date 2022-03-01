a2a3d90f0e456be3f6ed6e09a46dcac16f12a727
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-03-01 16:29:04 +0900

    added @incidents and @markers

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,14 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [ :home ]
-
+  skip_before_action :authenticate_user!, only: [:home]
   def home
+    # Had to add the following instances otherwise the map wouldn't work.
+    @incidents = policy_scope(Incident)
+    @markers = @incidents.geocoded.map do |incident|
+      {
+        lat: incident.latitude,
+        lng: incident.longitude,
+        id: incident.id
+      }
+    end
   end
 end
