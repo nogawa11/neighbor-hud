c1650b03cd6acfb72df315783be6db192f015bbe
Nicole Ogawa <nogawa11@gmail.com>
2022-03-03 18:01:57 +0900

    added search logic and filter logic to home page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,7 +2,15 @@ class PagesController < ApplicationController
   skip_before_action :authenticate_user!, only: [:home]
   def home
     # Had to add the following instances otherwise the map wouldn't work.
-    @incidents = policy_scope(Incident)
+    if params[:query].present?
+      @incidents = policy_scope(Incident).near(params[:query])
+    elsif params[:filter].present?
+      news_user_filter(params[:filter])
+    elsif params[:category].present?
+      category_filter(params[:category])
+    else
+      all_incidents
+    end
     @markers = @incidents.geocoded.map do |incident|
       {
         lat: incident.latitude,
@@ -10,5 +18,31 @@ class PagesController < ApplicationController
         id: incident.id
       }
     end
+  end
+
+  private
+
+  def news_user_filter(filter)
+    @incidents = policy_scope(Incident).where('user_id IS NULL') if filter == "crimenews"
+    @incidents = policy_scope(Incident).where('user_id IS NOT NULL') if filter == "userreports"
+  end
+
+  def date_filter(start_d, end_d)
+    @start_date = Date.parse(start_d)
+    @end_date = Date.parse(end_d)
+    @all_incidents = policy_scope(Incident)
+    @incidents = @all_incidents.where("incident_date >= ? AND incident_date <= ?", @start_date, @end_date)
+  end
+
+  def category_filter(category)
+    if category == "disturbing"
+      @incidents = policy_scope(Incident).tagged_with('Disturbing the Peace')
+    else
+      @incidents = policy_scope(Incident).tagged_with(category.capitalize)
+    end
+  end
+
+  def all_incidents
+    @incidents = policy_scope(Incident)
   end
 end
