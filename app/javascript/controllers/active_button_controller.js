eff51842772dfaa868887a37b758bba656739e32
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-03-04 23:35:11 +0900

    added ajax home

--- app/javascript/controllers/active_button_controller.js
+++ app/javascript/controllers/active_button_controller.js
@@ -1,14 +1,42 @@
-import { Controller } from "stimulus"
+import {
+  Controller
+} from "stimulus"
 
 export default class extends Controller {
-  static targets = []
+  static targets = ["button"]
 
   connect() {
-    console.log("Hello, Stimulus!");
+    this.buttonTargets.forEach((button) => {
+      button.addEventListener("click", (e) => {
+        this.fetchData(e);
+        this.buttonTargets.forEach((sibling) => {
+          sibling.classList.remove("active");
+        })
+        button.classList.add("active");
+      });
+    })
   }
 
-  setActive() {
-    console.log("Clicked");
+  fetchData(e) {
+    const options = {
+      method: "GET",
+      headers: {
+        'Accept': 'text/plain'
+      }
+    }
+    const path = this.#getFormattedPath(e.target);
+    fetch(`/?filter=${path}`, options).then((response) =>
+      response.text().then((responseText) => {
+        const map = document.querySelector("#mapbox");
+        map.outerHTML = responseText;
+        console.log("Done");
+      })
+    );
   }
 
+/* --------------------------------- Private -------------------------------- */
+  #getFormattedPath(button) {
+    const path = button.innerText.replace(/\s/g, '');
+    return path.toLowerCase();
+  }
 }
