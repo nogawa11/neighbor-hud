ec16481c1a1014d911d81c8b36eade255afeb76b
Amane Weston <amane.weston02@gmail.com>
2022-03-05 17:42:16 +0900

    disable a report button when the form is submitted

--- app/javascript/controllers/report-details_controller.js
+++ app/javascript/controllers/report-details_controller.js
@@ -1,10 +1,50 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["card", "button"]
+  static targets = ["card", "button", "location", "category", "title", "reportButton"]
+
+  previousLocation = '';
+  previousCategory = '';
+  previousTitle = '';
+
+  get location() {
+    return this.locationTarget.value;
+  }
+
+  get category() {
+    return this.categoryTarget.value;
+  }
+
+  get title() {
+    return this.titleTarget.value;
+  }
+
+  get isLocationChanged() {
+    return this.previousLocation !== this.location;
+  }
+
+  get isCategoryChanged() {
+    return this.previousCategory !== this.category;
+  }
+
+  get isTitleChanged() {
+    return this.previousTitle !== this.title;
+  }
+
+  get isFormValid() {
+    return (this.isLocationChanged || this.isCategoryChanged) || this.isTitleChanged;
+  }
+
+  set disableForm(value) {
+    this.reportButtonTarget.disabled = value;
+  }
 
   connect() {
     console.log("Hello, Stimulus!");
+    this.previousLocation = this.location;
+    this.previousCategory = this.category;
+    this.previousTitle = this.title;
+    this.disableForm = true;
   }
 
   openCard(event) {
@@ -13,7 +53,11 @@ export default class extends Controller {
     this.cardTarget.classList.toggle("open");
   }
 
-  create() {
-    console.log("Hello");
+  validateForm() {
+    this.disableForm = this.isFormValid ? false : true;
   }
+
+  // create() {
+  //   console.log("Hello");
+  // }
 }
