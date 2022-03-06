2d8d20f981059cb8f714519314dfe0c1ac2af0bb
Nicole Ogawa <nogawa11@gmail.com>
2022-03-06 21:40:13 +0900

    added close option to filters on feed

--- app/javascript/controllers/filter_controller.js
+++ app/javascript/controllers/filter_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "stimulus"
 
 export default class extends Controller {
-  static targets = ["card", "button", "closedCard"]
+  static targets = ["card", "button", "closedCard", "categories", "date", "feed"]
 
   connect() {
   }
@@ -10,5 +10,12 @@ export default class extends Controller {
     this.buttonTarget.classList.toggle("open")
     this.cardTarget.classList.toggle("open");
     this.closedCardTarget.classList.toggle("open");
+  }
+
+  closeFilter() {
+    this.buttonTarget.classList.toggle("open")
+    this.categoriesTarget.classList.toggle("open");
+    this.dateTarget.classList.toggle("open");
+    this.feedTarget.classList.toggle("open");
   }
 }
