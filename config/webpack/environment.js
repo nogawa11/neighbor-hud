f0e9860bd0881aba2c34fde90b93abc5a660a756
Ayanori Toyoda <rodrigotoyoda@outlook.com>
2022-03-08 00:12:40 +0900

    fixed pwa

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,7 @@
-const { environment } = require('@rails/webpacker')
+const { config, environment, Environment } = require("@rails/webpacker");
 // Preventing Babel from transpiling NodeModules packages
 environment.loaders.delete('nodeModules');
+const { resolve } = require("path");
+const WebpackerPwa = require("webpacker-pwa");
+new WebpackerPwa(config, environment);
 module.exports = environment
---
+++ public/client-51870b3ee1aeecb39606.js
@@ -0,0 +1,148 @@
+/******/ (function(modules) { // webpackBootstrap
+/******/ 	// The module cache
+/******/ 	var installedModules = {};
+/******/
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/
+/******/ 		// Check if module is in cache
+/******/ 		if(installedModules[moduleId]) {
+/******/ 			return installedModules[moduleId].exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = installedModules[moduleId] = {
+/******/ 			i: moduleId,
+/******/ 			l: false,
+/******/ 			exports: {}
+/******/ 		};
+/******/
+/******/ 		// Execute the module function
+/******/ 		modules[moduleId].call(module.exports, module, module.exports, __webpack_require__);
+/******/
+/******/ 		// Flag the module as loaded
+/******/ 		module.l = true;
+/******/
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/
+/******/
+/******/ 	// expose the modules object (__webpack_modules__)
+/******/ 	__webpack_require__.m = modules;
+/******/
+/******/ 	// expose the module cache
+/******/ 	__webpack_require__.c = installedModules;
+/******/
+/******/ 	// define getter function for harmony exports
+/******/ 	__webpack_require__.d = function(exports, name, getter) {
+/******/ 		if(!__webpack_require__.o(exports, name)) {
+/******/ 			Object.defineProperty(exports, name, { enumerable: true, get: getter });
+/******/ 		}
+/******/ 	};
+/******/
+/******/ 	// define __esModule on exports
+/******/ 	__webpack_require__.r = function(exports) {
+/******/ 		if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 			Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 		}
+/******/ 		Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 	};
+/******/
+/******/ 	// create a fake namespace object
+/******/ 	// mode & 1: value is a module id, require it
+/******/ 	// mode & 2: merge all properties of value into the ns
+/******/ 	// mode & 4: return value when already ns object
+/******/ 	// mode & 8|1: behave like require
+/******/ 	__webpack_require__.t = function(value, mode) {
+/******/ 		if(mode & 1) value = __webpack_require__(value);
+/******/ 		if(mode & 8) return value;
+/******/ 		if((mode & 4) && typeof value === 'object' && value && value.__esModule) return value;
+/******/ 		var ns = Object.create(null);
+/******/ 		__webpack_require__.r(ns);
+/******/ 		Object.defineProperty(ns, 'default', { enumerable: true, value: value });
+/******/ 		if(mode & 2 && typeof value != 'string') for(var key in value) __webpack_require__.d(ns, key, function(key) { return value[key]; }.bind(null, key));
+/******/ 		return ns;
+/******/ 	};
+/******/
+/******/ 	// getDefaultExport function for compatibility with non-harmony modules
+/******/ 	__webpack_require__.n = function(module) {
+/******/ 		var getter = module && module.__esModule ?
+/******/ 			function getDefault() { return module['default']; } :
+/******/ 			function getModuleExports() { return module; };
+/******/ 		__webpack_require__.d(getter, 'a', getter);
+/******/ 		return getter;
+/******/ 	};
+/******/
+/******/ 	// Object.prototype.hasOwnProperty.call
+/******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
+/******/
+/******/ 	// __webpack_public_path__
+/******/ 	__webpack_require__.p = "/";
+/******/
+/******/
+/******/ 	// Load entry module and return exports
+/******/ 	return __webpack_require__(__webpack_require__.s = "./app/javascript/service_workers/client.js");
+/******/ })
+/************************************************************************/
+/******/ ({
+
+/***/ "./app/javascript/service_workers/client.js":
+/*!**************************************************!*\
+  !*** ./app/javascript/service_workers/client.js ***!
+  \**************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+console.log("...");
+var CACHE_NAME = "v1-cached-assets";
+
+function onInstall(event) {
+  event.waitUntil(caches.open(CACHE_NAME).then(function prefill(cache) {
+    return cache.addAll(['<%= asset_path "application.js" %>', '<%= asset_path "application.css" %>', "/offline.html" // you get the idea ...
+    ]);
+  }));
+}
+
+function onActivate(event) {
+  console.log("[Serviceworker]", "Activating!", event);
+  event.waitUntil(caches.keys().then(function (cacheNames) {
+    return Promise.all(cacheNames.filter(function (cacheName) {
+      // Return true if you want to remove this cache,
+      // but remember that caches are shared across
+      // the whole origin
+      return cacheName.indexOf("v1") !== 0;
+    }).map(function (cacheName) {
+      return caches["delete"](cacheName);
+    }));
+  }));
+}
+
+function onFetch(event) {
+  // Fetch from network, fallback to cached content, then offline.html for same-origin GET requests
+  var request = event.request;
+
+  if (!request.url.match(/^https?:\/\/example.com/)) {
+    return;
+  }
+
+  if (request.method !== "GET") {
+    return;
+  }
+
+  event.respondWith(fetch(request) // first, the network
+  ["catch"](function fallback() {
+    caches.match(request).then(function (response) {
+      // then, the cache
+      response || caches.match("/offline.html"); // then, /offline cache
+    });
+  })); // See https://jakearchibald.com/2014/offline-cookbook/#on-network-response for more examples
+}
+
+self.addEventListener("install", onInstall);
+self.addEventListener("activate", onActivate);
+self.addEventListener("fetch", onFetch);
+
+/***/ })
+
+/******/ });
+//# sourceMappingURL=client-51870b3ee1aeecb39606.js.map
