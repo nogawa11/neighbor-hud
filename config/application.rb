9f72822e4a603ee8d4a63bc691c14d6ad36d5174
Nicole Ogawa <nogawa11@gmail.com>
2022-03-08 15:06:06 +0900

    changed assets initialize on precompile to false

--- config/application.rb
+++ config/application.rb
@@ -26,5 +26,6 @@ module Neighborhud
 
     config.active_job.queue_adapter = :sidekiq
     config.action_view.embed_authenticity_token_in_remote_forms = true
+    config.assets.initialize_on_precompile = false
   end
 end
