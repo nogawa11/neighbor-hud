ca44a17b1432b2fd4e4b3ecfebc2574264562572
Nicole Ogawa <nogawa11@gmail.com>
2022-03-09 19:40:40 +0900

    fixed uncommented line

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -14,7 +14,7 @@ class ApplicationController < ActionController::Base
   end
 
   def default_url_options
-    { host: ENV["neighbor-hud.com"] || "localhost:3000" }
+    { host: ENV["DOMAIN"] || "localhost:3000" }
   end
 
   private
